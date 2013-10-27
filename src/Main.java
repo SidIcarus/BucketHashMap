6ae393670848a475118f7a23a9171cf632292461
ras1668 <ras1668@rit.edu>
2013-10-27 17:36:57 -0400

    Basic code added in Main.java Nothing final here, just creating a bucket hash map with 50 key/value pairs, initializing three worker threads by passing in the map, and starting each of them.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		ConcurrentBucketHashMap bucketMap = new ConcurrentBucketHashMap(3);
+		for(int i = 0; i < 50; i++) {
+			bucketMap.put(i,i*42);
+		}
+		WorkerThread thread1 = new WorkerThread(bucketMap);
+		WorkerThread thread2 = new WorkerThread(bucketMap);
+		WorkerThread thread3 = new WorkerThread(bucketMap);
+		thread1.start();
+		thread2.start();
+		thread3.start();
 	}
 }
