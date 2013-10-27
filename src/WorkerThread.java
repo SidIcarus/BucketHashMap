8dad66ca756ce6e988e5405ffeafc445fc642d78
SidIcarus <app6141@gmail.com>
2013-10-27 17:42:02 -0400

    Stubbed it out a bit more.

--- src/WorkerThread.java
+++ src/WorkerThread.java
@@ -1,3 +1,14 @@
 public class WorkerThread extends Thread{
-
+	
+	private ConcurrentBucketHashMap map;
+	private int threadNum;
+	
+	public WorkerThread(ConcurrentBucketHashMap map, int threadNum){
+		this.map = map;
+		this.threadNum = threadNum;
+	}
+	
+	public void run(){
+		
+	}
 }
