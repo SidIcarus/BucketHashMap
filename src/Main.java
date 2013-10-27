f48119ab1c0c75148e4c7578d0414b36054ccebb
ras1668 <ras1668@rit.edu>
2013-10-27 17:39:48 -0400

    Updated WorkerThread initializations to reflect thread number.

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
 		for(int i = 0; i < 50; i++) {
 			bucketMap.put(i,i*42);
 		}
-		WorkerThread thread1 = new WorkerThread(bucketMap);
-		WorkerThread thread2 = new WorkerThread(bucketMap);
-		WorkerThread thread3 = new WorkerThread(bucketMap);
+		WorkerThread thread1 = new WorkerThread(bucketMap, 1);
+		WorkerThread thread2 = new WorkerThread(bucketMap, 2);
+		WorkerThread thread3 = new WorkerThread(bucketMap, 3);
+		System.out.println("Attempt to break the ConcurrentBucketHashMap started!");
 		thread1.start();
 		thread2.start();
 		thread3.start();
